9efccb9b2161d2e43bbc5e1a9fbd791d224259a8
MbakzArr <mbadaligab@gmail.com>
2024-05-06 23:23:58 +0200

    javascript 2

--- CodeTribe/Javascript.js
+++ CodeTribe/Javascript.js
@@ -3,7 +3,9 @@ var bio = ""
 var projects = []
 var educationalBackground = [
     {
-
+        institution: "University of Limpopo",
+        degree: "Bachelor of Science in Mathematical and Computer Science",
+        GraduationYear: 2024
     }
 ]
 
@@ -12,4 +14,22 @@ var contactInformation = {
     Call: "076 171 4986",
     Email: "Mbadaligab@gmail.com",
     Address: "Mankweng, Turfloop, University Road"
+}
+
+var personalInformation = {
+    DateofBirth: "July 29, 2000",
+    Email: "Mbadaligab@gmail.com",
+    Phone: "076 171 4986"
+} 
+
+var projects = {
+    project1 : "Bilding my home language bot",
+    project2 : "Configuring Networks in Packet tracer",
+    project3 : "Project 3: CHPC NICIS Student cluster competition"
+}
+
+var workExperience = {
+    experience1 : 
+    experience2 :
+    experience :  
 }
