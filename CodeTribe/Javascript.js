6f36ca41b37161ac75b2111988d04099683c0f3f
MbakzArr <mbadaligab@gmail.com>
2024-05-06 13:23:46 +0200

    javascript

---
+++ CodeTribe/Javascript.js
@@ -0,0 +1,15 @@
+var fullName = "Mbadaliga Arehone"
+var bio = ""
+var projects = []
+var educationalBackground = [
+    {
+
+    }
+]
+
+var contactInformation = {
+    Whatsapp: "076 171 4986",
+    Call: "076 171 4986",
+    Email: "Mbadaligab@gmail.com",
+    Address: "Mankweng, Turfloop, University Road"
+}
